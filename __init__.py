64e36db076aba46407f11228e4bdab318c321770
Àngel Àlvarez <angel@nan-tic.com>
2021-06-17 10:46:19 +0200

    add information_uom on purchase.product_supplier.price model

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from . import purchase
 def register():
     Pool.register(
         purchase.PurchaseLine,
+        purchase.ProductSupplierPrice,
         module='purchase_information_uom', type_='model')
     Pool.register(
         purchase.CreatePurchase,
