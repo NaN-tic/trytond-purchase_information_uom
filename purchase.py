8d474c30757a8e886e52cc1588b15ce5c7ba67b1
Àngel Àlvarez <angel@nan-tic.com>
2021-06-23 09:31:04 +0200

    fix on_change_with and depends

--- purchase.py
+++ purchase.py
@@ -65,12 +65,14 @@ class ProductSupplierPrice(metaclass=PoolMeta):
     currency_digits = fields.Function(fields.Integer('Currency Digits'),
         'on_change_with_currency_digits')
     product = fields.Function(fields.Many2One('product.product', 'Product'),
-        'get_product')
+        'on_change_with_product')
 
-    def get_product(self, name=None):
-        return (self.product_supplier.product and
+    @fields.depends('product_supplier', '_parent_product_supplier.product')
+    def on_change_with_product(self, name=None):
+        return (self.product_supplier and self.product_supplier.product and
             self.product_supplier.product.id)
 
+    @fields.depends('product_supplier', '_parent_product_supplier.product')
     def on_change_with_currency_digits(self, name=None):
         return 2
 
@@ -78,13 +80,14 @@ class ProductSupplierPrice(metaclass=PoolMeta):
     def default_info_unit_digits():
         return 2
 
-    @fields.depends('product')
+    @fields.depends('product', 'product_supplier',
+        '_parent_product_supplier.product')
     def on_change_with_info_unit_digits(self, name=None):
         if self.info_unit:
             return self.info_unit.digits
         return 2
 
-    @fields.depends('product')
+    @fields.depends('product_supplier', '_parent_product_supplier.product')
     def on_change_with_show_info_unit(self, name=None):
         if self.product and self.product.template.use_info_unit:
             return True
